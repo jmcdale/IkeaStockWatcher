db24e164db3665dde5e4f12ea3adcb5ac60cc9e3
Josh McDale <josh@jmcdale.com>
2021-03-07 15:28:35 -0600

    Add a handful of other items to the list.

--- app/src/main/java/com/jmcdale/ikea/watcher/local/Data.kt
+++ app/src/main/java/com/jmcdale/ikea/watcher/local/Data.kt
@@ -16,4 +16,39 @@ val DEFAULT_ITEMS = listOf(
         itemName = "TEST - LENNART Drawer unit, white",
         imageUrl = "https://www.ikea.com/us/en/images/products/lennart-drawer-unit-white__0395412_pe564513_s5.jpg?f=xxxs"
     ),
+    MainStockItem(
+        itemNumber = "502.655.02".cleanItemNumber(),
+        itemName = "SEKTION Base corner cabinet frame, white47x26x30",
+        imageUrl = "https://www.ikea.com/us/en/images/products/sektion-base-corner-cabinet-frame-white__0626401_pe692727_s5.jpg?f=xxxs"
+    ),
+    MainStockItem(
+        itemNumber = "004.260.55".cleanItemNumber(),
+        itemName = "AXSTAD Door, matt white18x30",
+        imageUrl = "https://www.ikea.com/us/en/images/products/axstad-door-matt-white__0650824_pe706549_s5.jpg?f=xxxs"
+    ),
+    MainStockItem(
+        itemNumber = "202.653.96".cleanItemNumber(),
+        itemName = "SEKTION Base cabinet frame, white18x24x30",
+        imageUrl = "https://www.ikea.com/us/en/images/products/sektion-base-cabinet-frame-white__0268604_pe406671_s5.jpg?f=xxxs"
+    ),
+    MainStockItem(
+        itemNumber = "002.654.39".cleanItemNumber(),
+        itemName = "SEKTION High cabinet frame, white18x24x90",
+        imageUrl = "https://www.ikea.com/us/en/images/products/sektion-high-cabinet-frame-white__0268551_pe406649_s5.jpg?f=xxxs"
+    ),
+    MainStockItem(
+        itemNumber = "302.653.86".cleanItemNumber(),
+        itemName = "SEKTION Base cabinet frame, white30x24x30",
+        imageUrl = "https://www.ikea.com/us/en/images/products/sektion-base-cabinet-frame-white__0268628_pe415274_s5.jpg?f=xxxs"
+    ),
+    MainStockItem(
+        itemNumber = "402.655.26".cleanItemNumber(),
+        itemName = "UTRUSTA Shelf, white36x14 3/4",
+        imageUrl = "https://www.ikea.com/us/en/images/products/utrusta-shelf-white__0268750_pe406747_s5.jpg?f=xxxs"
+    ),
+    MainStockItem(
+        itemNumber = "202.655.13".cleanItemNumber(),
+        itemName = "SEKTION Wall top cabinet frame, white36x24x20 ",
+        imageUrl = "https://www.ikea.com/us/en/images/products/sektion-wall-top-cabinet-frame-white__0285979_pe422910_s5.jpg?f=xxxs"
+    )
 )
